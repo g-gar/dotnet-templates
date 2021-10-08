c38ac57ba1ec048dde2054e1b4bcd10d255d7687
g-gar <g.gar.fern@hotmail.com>
2021-10-08 13:19:07 +0200

    added extensions folder and Cout extension

---
+++ library_git_test/extensions/base/GeneralPurposeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace extensions {
+    public static class GeneralPurposeExtensions{
+        public static void Cout(this object obj, params object[] args) { 
+            Console.WriteLine(obj.ToString(), args);
+        }
+    }
+}
